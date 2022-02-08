5a788c9150f8821139a86b87197c512bba133b57
Denis <Denis.Meyer@gmx.ch>
2022-02-08 10:41:06 +0100

    Added rain #6

---
+++ src/scripts/rain.js
@@ -0,0 +1,24 @@
+// number of drops created.
+var nbDrop = 200;
+
+// function to generate a random number range.
+function randRange(minNum, maxNum) {
+    return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+}
+
+// function to generate drops
+function createRain() {
+
+    for (i = 1; i < nbDrop; i++) {
+        var dropLeft = randRange(0, 1600);
+        var dropTop = randRange(-1000, 1400);
+
+        document.querySelector('.rain').innerHTML += '<div class="drop" id="drop' + i + '"></div>';
+        document.getElementById('drop' + i).style.left = dropLeft + "px";
+        document.getElementById('drop' + i).style.top = dropTop + "px";
+        // document.getElementById('drop' + i).style.top = dropTop;
+    }
+
+}
+// Make it rain
+createRain();
